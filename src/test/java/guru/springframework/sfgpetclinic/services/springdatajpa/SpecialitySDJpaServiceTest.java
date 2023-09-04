61462fd5964d7a0a664105502274835f1e355e24
dennis githinji <wakahiad@gmail.com>
2023-09-04 14:35:15 +0300

    inject mocks with mockito

---
+++ src/test/java/guru/springframework/sfgpetclinic/services/springdatajpa/SpecialitySDJpaServiceTest.java
@@ -0,0 +1,28 @@
+package guru.springframework.sfgpetclinic.services.springdatajpa;
+
+import guru.springframework.sfgpetclinic.model.Speciality;
+import guru.springframework.sfgpetclinic.repositories.SpecialtyRepository;
+import guru.springframework.sfgpetclinic.services.SpecialtyService;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+class SpecialitySDJpaServiceTest {
+    @Mock
+    SpecialtyRepository specialtyRepository;
+    @InjectMocks
+    SpecialitySDJpaService specialtyService;
+
+    @Test
+    void deleteById() {
+        specialtyService.deleteById(1l);
+    }
+
+    @Test
+    void testDelete() {
+        specialtyService.delete(new Speciality());
+    }
+}
